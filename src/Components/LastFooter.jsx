dec0a4e8252c306acca0f017b2bf85dff86cf4b5
FIRST_NAME LAST_NAMEAnasPirzada <anaspirzadaiub@gmail.com>
2024-11-09 01:17:25 +0500

    final changes

--- src/Components/LastFooter.jsx
+++ src/Components/LastFooter.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function LastFooter() {
   return (
     <>
-      <section className='px-5'>
+      <section className='px-5 py-3'>
         <div className=' flex justify-between'>
           <div>
             <h3>© 2024 Smart Quran Institute</h3>
@@ -16,8 +16,22 @@ function LastFooter() {
               rel='noopener noreferrer'
             >
               <img src='/Youtube.svg' className='w-10' alt='' />
+            </a>{' '}
+            <a
+              href='https://join.skype.com/invite/BOf0VvMKyAVe'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <img src='/skype.svg' className='w-10' alt='' />
             </a>
-            <img src='/instagram.svg' className='w-10' alt='' />
+            {/* <img src='/instagram.svg' className='w-10' alt='' /> */}
+            <a
+              href='https://www.facebook.com/profile.php?id=100083203658283'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <img src='/facebook-logo.svg' className='w-10' alt='' />
+            </a>
           </div>
           <div className=' flex gap-3'>
             <img src='/logos_mastercard.png' alt='logo' />
